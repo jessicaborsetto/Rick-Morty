b37a9d87ffa89e0ecd82f36cd0b911eeec64614a
jessicaborsetto <bjessi2002@gmail.com>
2024-02-19 10:51:42 +0100

    RM

--- RickMorty/src/App.jsx
+++ RickMorty/src/App.jsx
@@ -1,14 +1,28 @@
 import PicGallery from './components/PicGalley'
+import Header from './components/Header'
+import MySide from './components/MySide'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    <div className='container mx-auto'>
-    <PicGallery></PicGallery>
+      <div>
+        <Header></Header>
+      </div>
 
-    </div>
+
+
+      <div className=' grid grid-cols-6'>
+        <div className='col-start-1 col-end-1'>
+          <MySide />
+
+        </div>
+        <div className='col-start-2 col-end-7'>
+          <PicGallery ></PicGallery>
+        </div>
+
+      </div>
     </>
   )
 }
---
+++ RickMorty/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import gif from '../assets/download.gif'
+import logo from '../assets/Rick_and_Morty.svg.png'
+
+function Header(){
+    return(
+
+            <div className='relative mb-10'>
+                <img src={gif} alt="" className=' w-full h-[30rem] object-cover drop-shadow-2xl'/>
+                <img src={logo} alt="" className='w-[20rem] absolute bottom-5 right-5' />
+            </div>
+      
+    )
+}
+export default Header
